f2b18f58b2907f0906f923cce6f20a18209e1150
FcoRipJr <57280475+FcoRipJr@users.noreply.github.com>
2023-04-11 19:19:31 -0400

    Add files via upload

---
+++ routes copy.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const ContatosController = require('./controllers/ContatosController')
+const router = express.Router()
+
+router.get('/', ContatosController.index)
+router.post('/novo', ContatosController.salvarNovo)
+router.post('/excluir/:id', ContatosController.excluir)
+router.put('/editar/:id', ContatosController.salvarEditar)
+
+router.get('/novo', ContatosController.novo)
+router.get('/editar/:id', ContatosController.editar)
+
+module.exports = router
