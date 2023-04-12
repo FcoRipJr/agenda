07418e08c96361c562f420e0795064a36b53a88a
=FcoRipJr <=fcoripardojr@gmail.com>
2023-04-11 21:16:50 -0400

    crud completo

--- controllers/ContatosController.js
+++ controllers/ContatosController.js
@@ -24,21 +24,31 @@ class ContatosController {
 
     static async excluir(req, res) {
         const { id } = req.params
-        const contato = await contatos.destroy({
+        // const contato = await contatos.destroy({
+        //     where: {
+        //       id: id
+        //     }
+        //   })
+          const contato = await contatos.findByPk(id)
+          contato.destroy({
             where: {
               id: id
             }
           })
-        res.json(contato)
+        res.json(true)
     }
 
     static async salvarEditar(req, res) {
         const { id } = req.params
         const { nome, numero } = req.body
 
-        const contato = await contatos.update(
-            { nome: nome, numero: numero },
-            { where: { id: id } }
+        // const contato = await contatos.update(
+        //     { nome: nome, numero: numero },
+        //     { where: { id: id } }
+        // )
+        const contato = await contatos.findByPk(id)
+        contato.update(
+            { nome: nome, numero: numero }
         )
         res.json(contato)
 
