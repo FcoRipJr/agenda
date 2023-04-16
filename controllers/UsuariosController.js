d17b91ae3503fc0d889c8bbb85c0720dc30214b3
fcoripardojr <fcoripardojr@gmail.com>
2023-04-16 17:35:01 -0400

    add rota novasenha

--- controllers/UsuariosController.js
+++ controllers/UsuariosController.js
@@ -40,7 +40,7 @@ class UsuariosController {
 
     static async salvarEditar(req, res) {
         const { id } = req.params
-        const { nome, email, senha, data_nascimento, status } = req.body
+        const { nome, email, data_nascimento, status } = req.body
 
         // const usuario = await Usuarios.update(
         //     { nome: nome, email: email },
@@ -48,9 +48,38 @@ class UsuariosController {
         // )
         const usuario = await usuarios.findByPk(id)
         usuario.update(
-            { nome: nome, email: email, senha:senha, data_nascimento:data_nascimento, status:status }
+            { nome: nome, email: email, data_nascimento:data_nascimento, status:status }
         )
         res.json(usuario)
+
+    }
+
+    static async novaSenha(req, res) {
+        const { id } = req.params
+        const { senha, novaSenha, confirmarSenha } = req.body
+
+        const usuario = await usuarios.findByPk(id)
+        const sucesso = await bcrypt.compare(senha, usuario.senha)
+        if(sucesso){
+            if(novaSenha===confirmarSenha){
+                const salt = await  bcrypt.genSalt(12)
+                const password = await bcrypt.hash(novaSenha,salt)
+                usuario.update( { senha:password } )
+                res.json({
+                    succsses: 'Senha alterada com sucesso'
+                })
+            } else {
+                res.status(401).json({
+                    error: 'Senhas não são Identicas'
+                })
+            }
+            
+        } else {
+            res.status(401).json({
+                error: 'Senha atual Incorreta'
+            })
+        }
+        
 
     }
 }
