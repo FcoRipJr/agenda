bf532a0eae98de2c467f877bff586ac47c85461c
=FcoRipJr <=fcoripardojr@gmail.com>
2023-04-20 18:15:07 -0400

    add brlongsTo emails

--- models/emails.js
+++ models/emails.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.belongsTo(models.pessoas, {
+        as: 'pessoa',
+        foreignKey:'pessoas_id'
+      })
     }
   }
   emails.init({
